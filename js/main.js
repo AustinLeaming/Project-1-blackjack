f5d2e6f6d86ec945cb6cf24a6674cc22b8a91ad0
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-09-12 14:49:27 -0600

    updated css design, created getrandom button on page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-gameBoardEl = document.querySelector('#gameboard');
+//declarations
+const cardSuits = ['Spade', 'Heart', 'Club', 'Diamond']
+const cardNum = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
+
+//Dom declarations
+document.querySelector("#dealBtn").addEventListener("click", getRandom);
+
+// pick a random two values from card suit and card num 
+function getRandom(){
+    const ranIdxSuits = Math.floor(Math.random() * 4)
+    const ranIdxNum = Math.floor(Math.random() * 13)
+    const ranSuits = cardSuits[ranIdxSuits]
+    const ranNum = cardNum[ranIdxNum]
+    console.log(ranSuits)
+    console.log(ranNum)
+}
+
+// //button functions
+// dealEl.addEventListener
