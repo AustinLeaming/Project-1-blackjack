0f1df8d65f904ca77646aded4514b01003d66119
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-09-12 23:10:18 -0600

    card deck created, shuffled

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 //declarations
 const cardSuits = ['Spade', 'Heart', 'Club', 'Diamond']
 const cardNum = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
+const unshuffledDeck = [];
+const ranCard = ''
+const playerHand = []
 
-//Dom declarations
-document.querySelector("#dealBtn").addEventListener("click", getRandom);
+//Dom declarations & listeners
+document.querySelector("#dealBtn").addEventListener("click", startGame);
 
-// pick a random two values from card suit and card num 
-function getRandom(){
-    const ranIdxSuits = Math.floor(Math.random() * 4)
-    const ranIdxNum = Math.floor(Math.random() * 13)
-    const ranSuits = cardSuits[ranIdxSuits]
-    const ranNum = cardNum[ranIdxNum]
-    console.log(ranSuits)
-    console.log(ranNum)
+//create deck
+for (let i = 0; i < cardSuits.length; i++){
+    for (let x = 0; x < cardNum.length; x++){
+        let card = { Num: cardNum[x], Suit: cardSuits[i] }
+        unshuffledDeck.push(card)
+    }
 }
 
-// //button functions
-// dealEl.addEventListener
+//shuffle deck
+const shuffledDeck = unshuffledDeck.sort((a, b) => 0.5 - Math.random())
+
+
+//deal button pressed - deal first 2 cards to player, 1 to computer 
+function startgGame(){
+    let playerHand
+}
+
+
+// when hit button is pressed increase player hand 
+
