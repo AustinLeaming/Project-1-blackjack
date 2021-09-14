c2a78da7717b0de63c54ae30f3442c4deaa2d25d
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-09-14 14:17:13 -0600

    sanity save

--- js/main.js
+++ js/main.js
@@ -5,18 +5,25 @@ const unshuffledDeck = [];
 const playerHand = [];
 const computerHand = [];
 
-//variables to 
+//variables 
+let nextCardIndex = 4;
 let playerScore = 0;
 let computerScore = 0;
+let newCard = 0;
 
-//Dom declarations & listeners
+//Dom declarations
 const playerScoreEl = document.querySelector('#playerScore');
 const computerScoreEl = document.querySelector('#computerScore');
+//button elements & listeners
+//deal button
 const dealEl = document.querySelector('#dealBtn');
 dealEl.addEventListener('click', dealHand);
+//hit button
 const hitEl = document.querySelector('#hitBtn')
 hitEl.addEventListener('click', hitBtnFn);
-document.querySelector('#standBtn').addEventListener('click', standBtnFn);
+//stand button
+const standEl = document.querySelector('#standBtn')
+standEl.addEventListener('click', standBtnFn);
 
 //create deck
 for (let i = 0; i < cardSuits.length; i++){
@@ -31,15 +38,26 @@ const shuffledDeck = unshuffledDeck.sort((a, b) => 0.5 - Math.random())
 
 //deal button function - deal first 2 cards to player, 2 to computer keep 2nd card secret - render?
 function dealHand(){
-    dealEl.classList.add('inactive');
     playerHand.push(shuffledDeck[0], shuffledDeck[2]);
     computerHand.push(shuffledDeck[1], shuffledDeck[3]);
-    playerHandCheck();
-    computerHandCheck();
+    // removeCardFromShuffledDeck();
+    getButtonStyles();
+    firstHandDealPlayer();
+    firstHandDealComputer();
+}
+
+// function removeCardFromShuffledDeck(){
+//     shuffledDeck.pop(i)
+// }
+
+function getButtonStyles(){
+    dealEl.classList.add('inactive');
+    hitEl.classList.add('hitBtnStyle');
+    standEl.classList.add('standBtnStyle');
 }
 
 //check the value of the initial player hand
-function playerHandCheck(){
+function firstHandDealPlayer(){
     for (let i = 0; i < playerHand.length; i++){
         if (playerHand[i].Value === 'K' || playerHand[i].Value === 'Q' || playerHand[i].Value === 'J'){
             console.log(playerHand[i].Value, 'player hand')
@@ -53,11 +71,10 @@ function playerHandCheck(){
         }
         playerScoreEl.innerHTML = playerScore
     }
-
 }
 
 //check the value of the initial comptuer hand
-function computerHandCheck(){
+function firstHandDealComputer(){
     for (let i = 0; i < 1; i++){
         if (computerHand[i].Value === 'K' || computerHand[i].Value === 'Q' || computerHand[i].Value === 'J'){
             console.log(computerHand[i].Value, 'computer hand')
@@ -69,18 +86,59 @@ function computerHandCheck(){
             console.log(computerHand[i].Value, 'computer hand')
             computerScore = computerHand[i].Value + computerScore;
         }
-        computerScoreEl.innerHTML = computerScore
+        renderComputerScore()
     }
 }
 
-// when hit button is pressed increase player hand 
-function hitBtnFn(){
+//display the computer score on screen
+function renderComputerScore(){
+    computerScoreEl.innerHTML = computerScore
+    if (computerScore>21){
+        console.log('playerwins')
+    }
+}
 
+//reveal the second hand of the computer and add the value
+function getComputerSecondHand(){
+    computerScore = computerHand[1].Value + computerScore;
+    console.log('te')
+    renderComputerScore();
+}
+
+// when hit button is pressed increase player hand by next card in shuffled deck, keep track of index
+function hitBtnFn(){
+    playerHand.push(shuffledDeck[nextCardIndex]);
+    nextCardIndex ++;
+    getPlayerScore();
+}
+
+function getPlayerScore(){
+    newCard = playerHand.length - 1
+    if (playerHand[newCard].Value === 'K' || playerHand[newCard].Value === 'Q' || playerHand[newCard].Value === 'J'){
+        playerScore = playerScore + 10;
+    } else if(playerHand[newCard].Value === 'A'){
+        playerScore = playerScore + 11;
+    } else{
+        playerScore += playerHand[newCard].Value;
+    }
+    updatePlayerScoreInHTML();
+    if (playerScore>21){
+        busted();
+    }
+}
+
+function busted(){
+    
+    console.log('busted');
+}
+
+function updatePlayerScoreInHTML(){
+    playerScoreEl.innerHTML = playerScore;
 }
 
 // when stand button is pressed check computer hand to see if they need to hit or not 
 function standBtnFn(){
-
+    getComputerSecondHand()
 }
 
 // 
